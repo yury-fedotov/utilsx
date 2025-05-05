ae7a6f2a7a3091f87959dc08c65b6a7bc5c923d5
Yury Fedotov <yury_fedotov@mckinsey.com>
2025-05-05 18:14:51 -0400

    Find optimal number of bins

---
+++ src/utilsx/charts.py
@@ -0,0 +1,19 @@
+"""Utilities for working with charts, be it the UI app of just one-off EDA."""
+
+import typing as tp
+import math
+
+__all__ = ["find_optimal_nbins"]
+
+
+def find_optimal_nbins(values: tp.Sized) -> int:
+    """Find the optimal number of bins to represent values as a histogram.
+    Uses "The square root choice" algorithm, which is the default in MS Excel.
+
+    Args:
+        values: Array of values.
+
+    Returns:
+        The optimal number of bins to represent those as a histogram.
+    """
+    return int(math.sqrt(len(values)))
