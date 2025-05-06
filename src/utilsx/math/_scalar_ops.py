91920bbb287af5221ff9cd9b0f324826aa15df99
Yury Fedotov <yury_fedotov@mckinsey.com>
2025-05-06 14:14:32 -0400

    Improve docstrings of halve and double

--- src/utilsx/math/_scalar_ops.py
+++ src/utilsx/math/_scalar_ops.py
@@ -7,10 +7,24 @@ __all__ = [
 
 
 def halve(x: float) -> float:
-    """Divide a number by two. In other words, get its half."""
+    """Divide a number by two: in other words, get its half.
+
+    Args:
+        x: The number to divide by two.
+
+    Returns:
+        Result of dividing this number by literal integer two.
+    """
     return x / 2
 
 
 def double(x: float) -> float:
-    """Multiply a number by two. In other words, double it."""
+    """Divide a number by two: in other words, double it.
+
+    Args:
+        x: The number to multiply by two.
+
+    Returns:
+        Result multiplying this number by literal integer two.
+    """
     return x * 2
