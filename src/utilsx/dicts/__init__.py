0ef7833a40c0b14055cb5ea53d00303cf6b302c1
Yury Fedotov <yury_fedotov@mckinsey.com>
2025-05-09 14:53:39 -0400

    Change all imports to wildcards

--- src/utilsx/dicts/__init__.py
+++ src/utilsx/dicts/__init__.py
@@ -1,6 +1,6 @@
 """Utilities for working with dictionaries."""
 
 from .combination import *
-from .filtering import remove_pairs_with_zero_values
-from .modification import multiply_dict_values, rename_keys_in_nested_dict
+from .filtering import *
+from .modification import *
 from .sorting import *
