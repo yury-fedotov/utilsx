cadf7bf8c91c18237da0b0984e355492dbb6c1bc
Yury Fedotov <yury_fedotov@mckinsey.com>
2025-05-14 23:36:59 -0500

    Improve docstring in time test

--- tests/constants/test_time.py
+++ tests/constants/test_time.py
@@ -2,6 +2,6 @@ from utilsx.constants._time import HOURS_IN_YEAR, MINUTES_IN_WEEK
 
 
 def test_time() -> None:
-    """Test that time constants are mutually consistent."""
+    """Test that calculated time constants match known ground truth values."""
     assert HOURS_IN_YEAR == 8760
     assert MINUTES_IN_WEEK == 10080
