98890e38e63a758df9ad332a0a3da7fa63d4ae10
Yury Fedotov <yury_fedotov@mckinsey.com>
2025-05-15 22:03:34 -0500

    Re-calculate HUNDRED

--- src/utilsx/constants/_numbers.py
+++ src/utilsx/constants/_numbers.py
@@ -5,7 +5,7 @@ from typing import Final
 ONE: Final = 1
 TWO: Final = 2
 TEN: Final = 10
-HUNDRED: Final = TEN**2
+HUNDRED: Final = TEN**TWO
 THOUSAND: Final = TEN**3
 MILLION: Final = TEN**6
 BILLION: Final = TEN**9
