20a30954a92cbe5783c6d32c35da1a2f6629c335
Yury Fedotov <yury_fedotov@mckinsey.com>
2025-05-15 22:16:41 -0500

    Reorganize physics constants

--- src/utilsx/constants/_physics.py
+++ src/utilsx/constants/_physics.py
@@ -1,8 +1,16 @@
-"""Physical conversions and ratios."""
+"""Physical conversions and ratios.
+
+Naming convention is ``A_TO_B``, such that a value in units ``A``
+**multiplied** by ``A_TO_B`` becomes the same amount in units ``B``.
+"""
 
 from typing import Final
 
-GAL_IN_LITER: Final = 0.264172
-OZ_IN_GAL: Final = 128
-GRAMS_IN_LBS: Final = 453.592
-LBS_IN_KG: Final = 2.20462
+GAL_TO_LITER: Final = 3.785411784
+LITER_TO_GAL: Final = 1 / GAL_TO_LITER
+
+GAL_TO_OZ: Final = 128
+OZ_TO_GAL: Final = 1 / GAL_TO_OZ
+
+KG_TO_LBS: Final = 2.20462
+LBS_TO_KG: Final = 1 / KG_TO_LBS
