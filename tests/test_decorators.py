5383ca92cfdebd4c0cc5e031e54520639f81ce09
Yury Fedotov <yury_fedotov@mckinsey.com>
2025-05-21 08:42:01 -0500

    Add second test

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -3,12 +3,21 @@ from utilsx import return_element
 
 class TestReturnElement:
     def test_no_args_two_returns(self) -> None:
-        _default_return_value = (1, "la")
+        _example_return_value = (1, "la")
 
         def _example_function() -> tuple[int, str]:
-            return _default_return_value
+            return _example_return_value
 
         getter_of_first = return_element(0)(_example_function)
         getter_of_second = return_element(1)(_example_function)
-        assert getter_of_first() == _default_return_value[0]
-        assert getter_of_second() == _default_return_value[1]
+        assert getter_of_first() == _example_return_value[0]
+        assert getter_of_second() == _example_return_value[1]
+
+    def test_one_arg_two_returns(self) -> None:
+        def _get_square_and_cube(x: float) -> tuple[float, float]:
+            return x**2, x**3
+
+        get_square = return_element(0)(_get_square_and_cube)
+        get_cube = return_element(1)(_get_square_and_cube)
+        assert get_square(2) == 4
+        assert get_cube(2) == 8
