9c53d7c047fb5035832626936ed23069154c6b79
Yury Fedotov <yury_fedotov@mckinsey.com>
2025-05-31 15:40:07 -0400

    Add `count_na` pandas utility

---
+++ src/utilsx/pandas/__init__.py
@@ -0,0 +1,3 @@
+"""Utilities for enhancing your Pandas workflows."""
+
+from .missing import *
---
+++ src/utilsx/pandas/missing.py
@@ -0,0 +1,20 @@
+"""Utilities for analyzing, or dealing with missing values."""
+
+import pandas as pd
+
+__all__ = [
+    "count_na",
+]
+
+
+def count_na(df: pd.DataFrame) -> int:
+    """Get the total number of missing values in a DataFrame.
+
+    Args:
+        df: To count missing values in.
+
+    Returns:
+        The number of missing values.
+    """
+    count: int = df.isna().sum().sum()
+    return count
