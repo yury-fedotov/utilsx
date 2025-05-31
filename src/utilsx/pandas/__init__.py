a5b93762f93d6bf65e0fdb56d7280de7af6c5d41
Yury Fedotov <yury_fedotov@mckinsey.com>
2025-05-31 19:18:30 -0400

    Support a list of dependencies

--- src/utilsx/pandas/__init__.py
+++ src/utilsx/pandas/__init__.py
@@ -1,11 +1,17 @@
 """Utilities for enhancing your Pandas workflows."""
 
-try:
-    import pandas as pd
-except ImportError as e:
-    raise ImportError(
-        "Optional dependency 'pandas' is required for this feature.\n"
-        "Add 'utilsx[pandas]' to your requirements list."
-    ) from e
+from importlib import import_module
 
-from ._missing import *
+_MODULE_DEPENDENCIES = frozenset(("pandas",))
+
+for dependency in _MODULE_DEPENDENCIES:
+    try:
+        import_module(dependency)
+    except ImportError as e:
+        raise ImportError(
+            "Optional dependency group 'pandas' is required for this feature.\n"
+            "Add 'utilsx[pandas]' to your requirements list and install to virtual environment."
+        ) from e
+
+# This import should be after the dependency group check logic.
+from ._missing import *  # noqa: E402
