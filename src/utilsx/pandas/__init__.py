433322d71b72585701485871cabc2b3b56b255f2
Yury Fedotov <yury_fedotov@mckinsey.com>
2025-05-31 19:11:25 -0400

    Make the import warning work

--- src/utilsx/pandas/__init__.py
+++ src/utilsx/pandas/__init__.py
@@ -1,3 +1,11 @@
 """Utilities for enhancing your Pandas workflows."""
 
-from .missing import *
+try:
+    import pandas as pd
+except ImportError as e:
+    raise ImportError(
+        "Optional dependency 'pandas' is required for this feature.\n"
+        "Add 'utilsx[pandas]' to your requirements list."
+    ) from e
+
+from ._missing import *
